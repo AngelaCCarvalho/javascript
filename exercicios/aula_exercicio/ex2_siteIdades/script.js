7f766d24dcc326d9452e66756a9bd3aa748a35c2
Angela Carvalho <angeladanda@hotmail.com>
2026-01-17 21:30:53 -0300

    Atualizção

--- exercicios/aula_exercicio/ex2_siteIdades/script.js
+++ exercicios/aula_exercicio/ex2_siteIdades/script.js
@@ -5,19 +5,26 @@ botao.addEventListener('click', clicar)
 function clicar() {
 
     let inputData = document.querySelector('#data') // Variável que coleta o dado do usuário
+
+    // Se o campo estiver vazio, avise o usuário e pare a execução
+    if (inputData.value.length == 0) {
+        window.alert('[ERRO] Verifique os dados!')
+       return 
+    }
+
     let dataNasc = new Date(inputData.value); // variável de data com atribuição de valor no dado coletado do usuário.
 
 
     // Coleta de dados dos Dia, Mês e Ano.
-    let anoNasc = dataNasc.getUTCFullYear();
+    let anoNasc = dataNasc.getUTCFullYear(); //uso do UTC é importante por conta de fuso horário.
     let mesNasc = dataNasc.getUTCMonth() + 1; 
-    let diaNasc = dataNasc.getUTCDay();
-
+    let diaNasc = dataNasc.getUTCDate(); 
+    
     //Dados atuais do sistema, Dia, Mês, Ano.
     let ano = new Date();
     let anoAtual = ano.getFullYear();
     let mesAtual = ano.getMonth() + 1;
-    let diaAtual = ano.getDay();
+    let diaAtual = ano.getDate(); // Uso do Date() é para acessar dias 1-31, já o Day() acessa os dias da semana.
 
     // Cálculo Inicial
     let idade = anoAtual - anoNasc;
@@ -48,14 +55,32 @@ function clicar() {
 
     if( idade >= 1 && idade <= 14 && genero == 'Homem'){
 
-         txt.innerHTML = `Você é Menino e tem ${idade} anos.`
+         txt.innerHTML = `Você é Menino de ${idade} anos.`
          img.src = 'imagem/menino.png'
          
-    }else if (idade >= 1 && idade <= 14 && genero == 'Mulher')
+    }else if (idade >= 1 && idade <= 14 && genero == 'Mulher'){
 
-        txt.innerHTML = `Você é Menino e tem ${idade} anos.`
+        txt.innerHTML = `Você é um Menina de ${idade} anos.`
         img.src = 'imagem/menina.png'
-         
 
+    }else if(idade >= 18 && idade <= 59 && genero == 'Mulher'){
+         
+        txt.innerHTML = `Você é uma Mulher de ${idade} anos.`
+        img.src = 'imagem/mulher.png'
+
+    }else if(idade >= 18 && idade <= 59 && genero == 'Homem'){
+         
+        txt.innerHTML = `Você é um Homem de ${idade} anos.`
+        img.src = 'imagem/homem.png'
+
+    }else if(idade >= 60 && genero == 'Homem'){
+         
+        txt.innerHTML = `Você é um Homem de ${idade} anos.`
+        img.src = 'imagem/idoso.png' 
+
+    }else {         
+        txt.innerHTML = `Você é uma Mulher de ${idade} anos.`
+        img.src = 'imagem/idosa.png'   
+    }
 
 }
